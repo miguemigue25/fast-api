7055842b2a87276e50a02e00adf61f37a3f41021
Miguel <migueortiz0125@gmail.com>
2024-03-02 10:18:18 -0800

    modified chat responses table and have a create and get all responses

--- api/routers/response_routers.py
+++ api/routers/response_routers.py
@@ -1,43 +1,52 @@
-# from fastapi import (
-#     Depends,
-#     HTTPException,
-#     Response,
-#     APIRouter,
-#     Request,
-# )
-# from authenticator import authenticator
+from fastapi import (
+    Depends,
+    HTTPException,
+    Response,
+    APIRouter,
+    Request,
+)
+from authenticator import authenticator
 
-# from pydantic import BaseModel
-# from typing import Union, List
-# from queries.chat_responses import (
-#     ResponsesIn,
-#     ResponsesOut,
-#     ResponseRepository,
-#     Error,
-#     CreateResponseError
-# )
-
-
-# class HttpError(BaseModel):
-#     detail: str
+from pydantic import BaseModel
+from typing import Union, List
+from queries.chat_responses import (
+    ResponsesIn,
+    ResponsesOut,
+    ResponseRepository,
+    Error,
+    CreateResponseError
+)
 
 
-# router = APIRouter()
+class HttpError(BaseModel):
+    detail: str
 
 
-# @router.post("/responses", response_model=ResponsesOut | HttpError)
-# async def create_response(
-#     info: ResponsesIn,
-#     request: Request,
-#     response: Response,
-#     data: dict = Depends(authenticator.get_current_account_data),
-#     repo: ResponseRepository = Depends(),
-# ):
-#     try:
-#         responses = repo.create_response(info)
-#     except CreateResponseError:
-#         raise HTTPException(
-#             status_code=response.status_code == 404,
-#             detail="Could not create a response",
-#         )
-#     return responses
+router = APIRouter()
+
+
+@router.post("/responses", response_model=ResponsesOut | HttpError)
+async def create_response(
+    info: ResponsesIn,
+    request: Request,
+    response: Response,
+    data: dict = Depends(authenticator.get_current_account_data),
+    repo: ResponseRepository = Depends(),
+):
+    try:
+        user_id = data["user_id"]
+        responses = repo.create_response(info, user_id=user_id)
+    except CreateResponseError:
+        raise HTTPException(
+            status_code=response.status_code == 404,
+            detail="Could not create a response",
+        )
+    return responses
+
+@router.get("/responses", response_model=Union[List[ResponsesOut], Error])
+def get_all_responses(
+    repo: ResponseRepository = Depends(),
+    user_data: dict = Depends(authenticator.get_current_account_data),
+):
+
+    return repo.get_all_responses()
